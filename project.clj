32e2ad15867d0469a5c0e573dd748cf592044082
Christoph Frick <cf@ofnir.net>
2018-03-25 17:48:23 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.ofnir/relative-clj-http "0.1.0"
+(defproject net.ofnir/relative-clj-http "0.1.1-SNAPSHOT"
   :description "Helpers to provide defaults and relative modifications around clj-http"
   :url "https://github.com/christoph-frick/relative-clj-http"
   :license {:name "MIT"
