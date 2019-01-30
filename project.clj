6da0e52279999fd74037fc48192a51e11de0cf46
Christoph Frick <cf@ofnir.net>
2019-01-30 22:10:05 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/christoph-frick/relative-clj-http"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.9.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-http "3.9.1"]]
   :plugins [[lein-codox "0.10.3"]]
   :deploy-repositories [["releases" :clojars]]
   :codox {:output-path "docs"})
